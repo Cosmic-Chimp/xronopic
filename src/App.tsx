a114ada100063c3da2497b9cc7ded169fc3dd909
rueben-consnetcoz <rubzdebruyn@gmail.com>
2025-02-21 16:34:08 +0200

    chore(styling and ui-library): install and setup shadcn
    install and setup shadcn, along with tailwind config changes and added ONE component from shadcn -
    Button

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,15 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
+    const clicked = () => {
+        alert("'clicked'");
+    };
     return (
         <>
             <div className="flex justify-center items-center min-h-screen bg-gray-100">
                 <h1 className="text-5xl font-bold text-blue-300">
-                    Hallo, World!
+                    <Button onClick={clicked}>Hallo, World!</Button>
                 </h1>
             </div>
         </>
